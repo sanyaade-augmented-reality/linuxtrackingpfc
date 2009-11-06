56f110712c918073b81b8780479439cad578a701
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2009-11-06 14:30:23 +0000

    modificaciones en cube_tracker para aceptar las medidas del tracker en metros (segun el formato vrpn_tracker)

--- tests/cube_tracker.C
+++ tests/cube_tracker.C
@@ -10,6 +10,8 @@
    GLfloat obsx;
    GLfloat obsy;
    GLfloat obsz;
+   GLfloat scr2mdl;
+
 
   // Frame number y mensaje a mostrar
    GLint framen;
@@ -22,9 +24,6 @@ void init(void)
 {
    glClearColor (0.0, 0.0, 0.0, 0.0);
    glShadeModel (GL_FLAT);
-   obsx = 0.0;
-   obsy = 0.0;
-   obsz = 16.0;
 }
 
 void *font = GLUT_BITMAP_TIMES_ROMAN_24;
@@ -42,9 +41,6 @@ void display(void)
 
    GLfloat znear =1.0;
    GLfloat zfar =100.0;
-   /*obsx = -8.0;
-   obsy = 5.0;
-   obsz = 16.0;*/
    GLfloat frleft=znear*(-8.0-obsx)/obsz;
    GLfloat frright=znear*(8.0-obsx)/obsz;
    GLfloat frup=znear*(-5.0-obsy)/obsz;
@@ -94,11 +90,11 @@ void display(void)
 
    glColor3f(1.0, 1.0, 1.0);
    char buffer[160];
-   sprintf(buffer, "X %f", obsx);   
+   sprintf(buffer, "X %f", obsx/scr2mdl);   
    output(-7,-3.5,buffer ); 
-   sprintf(buffer, "Y %f", obsy);   
+   sprintf(buffer, "Y %f", obsy/scr2mdl);   
    output(-7,-4.0,buffer ); 
-   sprintf(buffer, "Z %f", obsz);   
+   sprintf(buffer, "Z %f", obsz/scr2mdl);   
    output(-7,-4.5,buffer ); 
 
 
@@ -111,52 +107,25 @@ void display(void)
    glutSwapBuffers(); //swap the buffers
 }
 
-void reshape (int w, int h)
-{
+void reshape (int w, int h){
    glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
-   /*glMatrixMode (GL_PROJECTION);
-   glLoadIdentity ();
-   glFrustum (-8.0, 8.0, -4.5, 4.5, 1.5, 20.0);
-   glMatrixMode (GL_MODELVIEW);*/
 }
-void keyboard(unsigned char key, int x, int y)
-{
+
+void keyboard(unsigned char key, int x, int y){
    switch (key) {
       case 27:
 	 glutLeaveGameMode(); //set the resolution how it was
          exit(0);
          break;
-      /*case 119: // w
-	 obsz-=0.1;
-	 glutPostRedisplay();
-	 break;
-      case 115: // s
-	 obsz+=0.1;
-	 glutPostRedisplay();
-	 break;
-      case 97: // a
-	 obsx-=0.1;
-	 glutPostRedisplay();
-	 break;
-      case 100: // d
-	 obsx+=0.1;
-	 glutPostRedisplay();
-	 break;
-      case 120: // x
-	 obsy-=0.1;
-	 glutPostRedisplay();
-	 break;
-      case 32: // space
-	 obsy+=0.1;
-	 glutPostRedisplay();
-	 break;*/
       default:
-	printf("Key %i not supported\n", key);
 	break;
    }
 }
 
+
 // Parte de vrpn
+
+
 vrpn_Tracker_Remote *tkr;
 
 void checktracker(){
@@ -165,13 +134,13 @@ void checktracker(){
 
 void    VRPN_CALLBACK handle_tracker(void *userdata, const vrpn_TRACKERCB t)
 {
-  /*printf("handle_tracker\tSensor %d is now at (%g,%g,%g)\n", 
-	 t.sensor,
-	 t.pos[0], t.pos[1], t.pos[2]);*/
-   obsx = t.pos[0];
-   obsy = t.pos[1];
-   obsz = t.pos[2]+16.0;
-  glutPostRedisplay();
+  
+   // t.sensor es la variable que da el numero de sensor
+   // en este ejemplo no se usa xq se ha registrado el callback para ejecutarse solo con el sensor0
+   obsx = t.pos[0]*scr2mdl;
+   obsy = t.pos[1]*scr2mdl;
+   obsz = (t.pos[2]+0.5)*scr2mdl;
+   glutPostRedisplay();
 }
 // Main y funciones auxiliares
 int main(int argc, char** argv)
@@ -181,7 +150,12 @@ int main(int argc, char** argv)
    tkr->register_change_handler(NULL, handle_tracker,0);
 
    framen=0;
-   //sprintf(mensaje,"Keys: w a s d x space esc\n");
+   GLfloat scrx = 0.52;
+   scr2mdl= 16.0/scrx; // ratio de conversion de medidas reales a las del modelo
+   obsx = 0.0;
+   obsy = 0.0;
+   obsz = 0.5*scr2mdl;
+   
    sprintf(mensaje,"Perspective adjustement via Tracker0@localhost (esc to exit)");
    
    glutInit(&argc, argv);
@@ -196,7 +170,6 @@ int main(int argc, char** argv)
    glutDisplayFunc(display); 
    glutReshapeFunc(reshape);
    glutKeyboardFunc(keyboard);
-   //glutIdleFunc(glutPostRedisplay);
    glutIdleFunc(checktracker);
    glutMainLoop();
    return 0;
