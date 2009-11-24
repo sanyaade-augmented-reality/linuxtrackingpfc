507c7976eeff07c8f8ebaf0ea7b01935feca51cd
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2009-11-24 16:34:25 +0000

    cambiado el obsz por defecto al crear un cliente, me habia dejado 10.5 en vez de 0.5 al hacer alguna prueba

--- client/TrackingPFC_client.C
+++ client/TrackingPFC_client.C
@@ -16,7 +16,7 @@ void TrackingPFC_client_callback(void *userdata, const vrpn_TRACKERCB t){
 TrackingPFC_client::TrackingPFC_client(const char* tname, void (cbfx)(TrackingPFC_client*)){
   obsx=0;
   obsy=0;
-  obsz=10.5;// para que si no hay tracker podemos ver algo, asumimos que no tenemos pegada la nariz a la pantalla
+  obsz=0.5;// para que si no hay tracker podemos ver algo, asumimos que no tenemos pegada la nariz a la pantalla
   
 
   alive=1;
