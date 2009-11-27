2e77872f799e98450183499bcf9372b4366f221f
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2009-11-27 18:31:16 +0000

    Aádido un semaforo a data para evitar errores en accesos simultaneos a los datos.

--- client/TrackingPFC_data.C
+++ client/TrackingPFC_data.C
@@ -3,6 +3,7 @@ TrackingPFC_data::TrackingPFC_data(float x, float y, float z){
   obsx=x;
   obsy=y;
   obsz=z;
+  lock = new pthread_mutex_t();
 }
 
 // Destructora
@@ -11,14 +12,18 @@ TrackingPFC_data::~TrackingPFC_data(){
 
 float* TrackingPFC_data::getlastpos(){
   float* res= new float[3];
+  pthread_mutex_lock( lock );
   res[0]=obsx;
   res[1]=obsy;
   res[2]=obsz;
+  pthread_mutex_unlock( lock );
   return res;
 }
 
 void TrackingPFC_data::setnewpos(float x, float y, float z){
+  pthread_mutex_lock( lock );
   obsx=x;
   obsy=y;
   obsz=z;
+  pthread_mutex_unlock( lock );
 }
