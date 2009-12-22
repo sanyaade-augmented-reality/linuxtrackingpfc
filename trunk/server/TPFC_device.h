f983415044527806c854f7a877c7a8093b80a1ff
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2009-12-22 17:09:02 +0000

    Primer push de los archivos del servidor

---
+++ trunk/server/TPFC_device.h
@@ -0,0 +1,72 @@
+#ifndef TPFC_DEVICE_
+#define TPFC_DEVICE_
+
+#include <stdio.h>
+#include <TrackingPFC_data.h>
+#include <vrpn_Tracker.h>
+#define TPFC_DEVICE_MAX_LISTENERS 10
+#define TPFC_STOP 0
+#define TPFC_RUN 1
+#define TPFC_PAUSE 2
+
+class TPFC_device{
+  private:
+    // id asignada a este dispositivo
+    int id; 
+
+    // Lista de los demas dispositivos a los que hay que avisar si hay un update
+    TPFC_device* listeners[TPFC_DEVICE_MAX_LISTENERS];
+    int registered_listeners;
+
+    // vrpn_Tracker server
+    vrpn_Tracker_Server * server;
+    //vrpn_Connection * connection;
+    
+
+  protected:
+    // apuntador al buffer de datos
+    TrackingPFC_data * data;
+    // flag de funcionamiento. Estados: TPFC_RUN, TPFC_STOP, TPFC_PAUSE
+    int running; 
+
+
+    
+
+
+  public:
+    
+    // consctructora y creadora
+    TPFC_device(int ident);
+    ~TPFC_device();
+
+    // registra otro device como listener. devuelve 0 si es correcto, -1 si ha habido un error
+    int report_to(TPFC_device*);
+    // funcion que indica que hay nuevos datos desde uno de los inputs
+    virtual void report_from(int id);
+  
+    // devuelve el puntero a los datos
+    TrackingPFC_data * getdata();
+    // devuelve el id
+    int idnum();
+    
+    // funciones para detener temporalmente o definitivamente los dispositivos
+    void pause();
+    void unpause();
+    virtual void stop();
+    
+
+    // consultoras sobre el estado del funcionamiento
+    int alive(); // devuelve 1 si el estado no es TPFC_STOP
+    int working(); // devuelve 1 si el estado es TPFC_RUN
+
+    // función para avisar a los listeners
+    void report();
+
+    // función para activar el envio de datos via vrpn_tracker al hacer report()
+    int settracker(vrpn_Connection *, const char*);
+
+    
+    
+};
+
+#endif /*TPFC_DEVICE_*/
---
+++ trunk/server/TPFC_device_opencv_face.h
@@ -0,0 +1,40 @@
+#ifndef TPFC_DEVICE_OPENCV_FACE_
+#define TPFC_DEVICE_OPENCV_FACE_
+
+#include <cv.h>
+#include <highgui.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+#include <time.h>
+#include <ctype.h>
+
+
+
+#include <pthread.h>
+#include <TrackingPFC_data.h>
+#include "TPFC_device.h"
+#define TPFC_DEVICE_MAX_LISTENERS 10
+
+class TPFC_device_opencv_face : public TPFC_device{
+  private:
+    pthread_t facedetect_thread; // thread que se encarga del facedetect
+    int cam;
+
+
+  public:
+    // consctructora y creadora
+    TPFC_device_opencv_face(int ident, int cam);
+    ~TPFC_device_opencv_face(); 
+    void stop();
+    int camera();
+};
+
+void *tpfcdevopencvfacedetect(void * t);
+
+#endif /*TPFC_DEVICE_OPENCV_FACE_*/
