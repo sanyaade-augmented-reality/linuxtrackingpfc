3dc2432298e59055f447a700607550c6873d6c1a
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-05 18:06:50 +0000

    integrado el resto de funciones auxiliares de los wiimotes como funciones estaticas de la clase

--- trunk/server/TPFC_device_wiimote.h
+++ trunk/server/TPFC_device_wiimote.h
@@ -3,6 +3,7 @@
 
 #include "TPFC_device.h"
 #include <cwiid.h>
+#include <functional>
 #define toggle_bit(bf,b)	\
 	(bf) = ((bf) & b)		\
 	       ? ((bf) & ~(b))	\
@@ -20,9 +21,14 @@ class TPFC_device_wiimote : public TPFC_device{
     // consctructora y creadora
     TPFC_device_wiimote(int ident);
     ~TPFC_device_wiimote();
+
+    static void wiimote_callback(cwiid_wiimote_t *, int , union cwiid_mesg mesg[], struct timespec *);
     
     static void err(cwiid_wiimote_t *, const char *, va_list);
     static void set_rpt_mode(cwiid_wiimote_t *, unsigned char);
+
+    static void registerwiimote(cwiid_wiimote_t *, TPFC_device_wiimote* );
+    static TPFC_device_wiimote* getwiimotedev(cwiid_wiimote_t *wiimote);
 
 };
 
