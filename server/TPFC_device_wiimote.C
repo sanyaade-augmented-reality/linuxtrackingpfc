2452afb4ea2d42c9f6c8c41232b9ffeeca8a8f42
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-07 16:10:46 +0000

    El dispositivo wiimote ahora guarda todos los puntos por separado en los datos (aun no se ven cambios evidentes ya que 3dfrom2d solo usa el primer punto)

--- server/TPFC_device_wiimote.C
+++ server/TPFC_device_wiimote.C
@@ -20,57 +20,45 @@ TPFC_device_wiimote* TPFC_device_wiimote::getwiimotedev(cwiid_wiimote_t *wiimote
 // Callback del wiimote
 void TPFC_device_wiimote::callback(cwiid_wiimote_t *wiimote, int mesg_count,
                     union cwiid_mesg mesg[], struct timespec *timestamp){
-	int i, j;
-	int valid_source;
-	int xacum, yacum;
-	float x,y;
-	for (i=0; i < mesg_count; i++){
-		switch (mesg[i].type) {
-		  case CWIID_MESG_IR:
-			  valid_source = 0;
-			  xacum=0;
-			  yacum=0;
-			  for (j = 0; j < CWIID_IR_SRC_COUNT; j++) {
-				  if (mesg[i].ir_mesg.src[j].valid) {
-					  valid_source++;
-					  xacum+=mesg[i].ir_mesg.src[j].pos[CWIID_X];
-					  yacum+=mesg[i].ir_mesg.src[j].pos[CWIID_Y];
-				  }
-			  }
-			  //printf("Callback de %i\n", cwiid_get_id(wiimote));
-			  
-			  if (valid_source>0) {
-			    // el wiimote es de ~ 1024x768
-			    x=512.0-(float(xacum)/float(valid_source));
-			    y=(float(yacum)/float(valid_source))-384.0;
-			    x=x/512.0;
-			    y=y/384.0;
-			    x=x/2.0;
-			    y=y/2.0;
-			    float* aux= new float[2];
-			    aux[0]=x;
-			    aux[1]=y;
-			    getwiimotedev(wiimote)->getdata()->setnewdata(aux);
-			    getwiimotedev(wiimote)->report();
-			    //printf("%f, %f\n",x,y);
-			    /*position[0]=x;
-			    position[1]=y;
-			    position[2]=0.5;
-			    vrpn_gettimeofday(&current_time, NULL);
-			    nt->report_pose(0,current_time, position, quaternion);
-			    nt->mainloop();
-			    connection->mainloop();*/
-			  }else{
-			    // en esta toma de datos no hay datos que reportar
-			    getwiimotedev(wiimote)->getdata()->setnodata();
-			    getwiimotedev(wiimote)->nullreport();
-			  }
-			  break;
-		  default:
-			  printf("Unknown Report");
-			  break;
-		}
+  int i, j;
+  int valid_source;
+  float x,y;
+  for (i=0; i < mesg_count; i++){
+    switch (mesg[i].type) {
+      case CWIID_MESG_IR:
+	valid_source = 0;
+	for (j = 0; j < CWIID_IR_SRC_COUNT; j++) {
+	  if (mesg[i].ir_mesg.src[j].valid) {
+	    valid_source++;
+	
+	    x=(512.0-mesg[i].ir_mesg.src[j].pos[CWIID_X])/512.0;
+	    y=(mesg[i].ir_mesg.src[j].pos[CWIID_Y]-384.0)/384.0;
+	    x=x/2.0;
+	    y=y/2.0;
+	  float* aux= new float[2];
+	  aux[0]=x;
+	  aux[1]=y;
+	  if (valid_source==1) // es el primer punto
+	    getwiimotedev(wiimote)->getdata()->setnewdata(aux);
+	  else // no es el primer punto, debemos usar el report existente
+	    getwiimotedev(wiimote)->getdata()->setmoredata(aux);
+	  }
 	}
+	
+	if (valid_source>0) {
+	  // los datos se han guardado en el bucle anterior, solo falta reportar
+	  getwiimotedev(wiimote)->report();
+	}else{
+	  // en esta toma de datos no hay datos que reportar
+	  getwiimotedev(wiimote)->getdata()->setnodata();
+	  getwiimotedev(wiimote)->nullreport();
+	}
+	break;
+      default:
+	printf("Unknown Report");
+	break;
+    }
+  }
 }
 
 
