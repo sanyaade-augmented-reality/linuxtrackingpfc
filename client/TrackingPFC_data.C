080354fb84cd11a99503ec6c088e2838148ae386
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-07 12:29:33 +0000

    Cambiada la estructura interna de data, ahora es un buffer de una estructura compleja, no solo de floats. Deberia ser 100% retrocompatible con todo el codigo anterior Aun no se usan las nuevas caracteristicas

--- client/TrackingPFC_data.C
+++ client/TrackingPFC_data.C
@@ -1,18 +1,21 @@
 #include "TrackingPFC_data.h"
-TrackingPFC_data::TrackingPFC_data(TPFCdatatype t, int s){
-  lock = new pthread_mutex_t();
-  type = t;
-  size = s;
 
+// Constructora
+TrackingPFC_data::TrackingPFC_data(TPFCdatatype t, int s){
+  lock = new pthread_mutex_t(); // inicializamos el semaforo
+  type = t; // guardamos el tipo de datos
+  size = s; // guardamos el tamaño del buffer
+
+  // configuramos el tamaño de los datos segun su tipo
   if (type==TPFCDATA2D) dsize=2;
   else if (type==TPFCDATA2DSIZE) dsize=3;
   else if (type==TPFCDATA3D) dsize= 3;
   else if (type==TPFCDATA3DORI) dsize= 7;
 
-  ind =-1;
-  count =0;
-  data = (float*)malloc(size*dsize*sizeof(float));
-
+  ind =-1;  // ponemos el indice a -1 (al rellenar la primera posición se pondrá a 0)
+  count =0; // 0 datos recibidos
+  data = new datachunk*[size]; // creamos el vector de apuntadores a los datachunks
+  
   // ponemos los datos por defecto
   // aunque para el tipo de datos se guarden menos de 7 floats, setnewdata se encarga de copiar
   // solo los necesarios
@@ -25,56 +28,77 @@ TrackingPFC_data::TrackingPFC_data(TPFCdatatype t, int s){
   defaultdata[5]=0.0;
   defaultdata[6]=0.0;
   setnewdata(defaultdata);
-  
 }
 
 // Destructora
 TrackingPFC_data::~TrackingPFC_data(){
+  // libreramos el espacio de memoria de los datachunks
+  for (int i =0; i < size; i++)
+    free(data[i]);
+  // y por ultimo destruimos el vector
   free(data);
 }
 
+// consultora simple que devuelve un vector de floats con los datos de la ultima posicion (+orientacion)
 float* TrackingPFC_data::getlastpos(){
+  // creamos el vector que devolveremos
   float* res= new float[dsize];
-  pthread_mutex_lock( lock );
-  int desp = ind*dsize;
+  pthread_mutex_lock( lock ); // obtenemos acceso exclusivo
+  // copiamos los datos
   for (int i =0; i<dsize; i++){
-    res[i]=data[desp];
-    desp++;
+    res[i]=data[ind]->data[i];
   }
-  pthread_mutex_unlock( lock );
+  pthread_mutex_unlock( lock ); // liberamos el acceso
   return res;
 }
 
-// copia la orientacion de los ultimos datos, modificando la posición.
+// copia la orientacion de los ultimos datos (si la hay), modificando la posición.
 // aunque está pensado para funcionar con datos de tamaño 3 o mas, funcionara con los de 2
 // ignorando el 3r argumento
 void TrackingPFC_data::setnewpos(float x, float y, float z){
-  float* aux = getlastpos();
-  pthread_mutex_lock( lock );
+  float* aux;
+  // si los datos tienen orientacion, obtenemos la ultima posición conocida para poder copiarla
+  if (dsize>3)
+    aux = getlastpos();
+  // si no, simplemente creamos un vector auxiliar (para no llamar a getlastpost, que bloquea el acceso a los datos)
+  else 
+    aux = new float[dsize];
+  // y rellenamos el vector
   aux[0]=x;
   aux[1]=y;
   if (dsize >2)
     aux[2]=z;
-  pthread_mutex_unlock( lock );
+  // por ultimo, guardamos esos nuevos datos
   setnewdata(aux);
 }
 
+// añade un nuevo chunk a los datos, con tiempo actual y los datos de d
+// no comprueba que los datos sean del tamaño correcto (que debe ser ==dsize)
 void TrackingPFC_data::setnewdata(float* d){
-  pthread_mutex_lock( lock );
+  pthread_mutex_lock( lock ); // obtenemos acceso exclusivo
+  // aumentamos el indice
   ind=(ind+1)%size;
-  int desp = ind*dsize;
-  for (int i =0; i<dsize; i++){
-    data[desp]= d[i];
-    desp++;
-  }
+  // destruimos el datachunk anterior
+  if (count>size)
+    free(data[ind]);
+  // creamos un nuevo vector para los datos
+  float* aux = new float[dsize];
+  // y lo rellenamos
+  for (int i =0; i<dsize;i++)
+    aux[i]=d[i];
+  // creamos un datachunk nuevo insertandolo en el buffer
+  data[ind]= new datachunk(aux);
+  // incrementamos el contador
   count++;
-  pthread_mutex_unlock( lock );
+  pthread_mutex_unlock( lock ); // liberamos el acceso exclusivo
 }
 
-int TrackingPFC_data::datatype(){
+// devuelve el tipo
+TPFCdatatype TrackingPFC_data::datatype(){
   return type;
 }
 
+// devuelve el tamaño de los datos segun el tipo
 inline int TrackingPFC_data::datasize(){
   return dsize;
 }
--- client/TrackingPFC_data.h
+++ client/TrackingPFC_data.h
@@ -3,34 +3,72 @@
 
 #include <pthread.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
 enum TPFCdatatype { TPFCDATA2D, TPFCDATA2DSIZE, TPFCDATA3D, TPFCDATA3DORI};
 
 class TrackingPFC_data{
+  // struct basico de la información relativa a un punto
+  public:
+    struct datachunk{
+      float* data;
+      clock_t time;
+      int tag;
+      bool valid;
+      datachunk* next;
+
+      // constructora
+      datachunk(float* f){
+	data= f;
+	time=clock();
+	tag=0;
+	valid=false;
+	next=NULL;
+      }
+      // destructora
+      ~datachunk(){
+	// si hay datos, los destruimos
+	if (data!=NULL)
+	  free(data);
+	// si hay mas datachunks en el mismo report, los destruimos
+	if (next!=NULL)
+	  free(next);
+      }
+      
+    };
+
   private:
     // tipo de datos almacenados y tamaño del buffer
-    TPFCdatatype type;
-    int size;
-    int dsize;
+    TPFCdatatype type; // tipo
+    int size;	// cantidad de reports del buffer
+    int dsize;	// tamaño del vector de datos (relativos a posición y orientacion)
     
     // Datos almacenados
-    float *data; 
+    datachunk** data; //vector de apuntadores a datachunk, debe tener tamaño size
     // indices que controlan el bufer
     int ind; // indice que apunta al primer dato valido del buffer
     int count; // total de datos recibidos
 
-    pthread_mutex_t* lock;
+    pthread_mutex_t* lock; // semaforo para la exclusión mutua
 
-  public:  
+  public:
+    // constructora y destructora
     TrackingPFC_data(TPFCdatatype type=TPFCDATA3DORI, int size=1);
     ~TrackingPFC_data();
 
     //devuelve el tipo de datos almacenado
-    int datatype();
+    TPFCdatatype datatype();
     // devuelve el tamaño de los datos que se guardan (de cuanto es el vector que se devuelve)
     int datasize();
 
+    // consultoras y escritoras simples (para reports con 1 solo punto por report)
+    // devuelve el vector de floats de posicion/orientacion de la ultima posicion reportada
     float* getlastpos();
-    void setnewpos(float, float, float);
+    // añade una nueva posición (copiando la orientación de la anterior posicion, si la hay)
+    // funciona tb para datos 2d, simplemente ignora el 3r float
+    void setnewpos(float, float, float f = 0.0);
+    // añade un nuevo report con los datos de d, d debe tener tamaño dsize
     void setnewdata(float* d);
 
 };
