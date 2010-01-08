54a267f69f9ced763beb63b83f566dadc72fecbf
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-08 22:28:29 +0000

    Comentadas las cabeceras cambiado el acceso de todas las funciones internas a privado

--- server/TPFC_device_wiimote.h
+++ server/TPFC_device_wiimote.h
@@ -11,16 +11,31 @@
 
 class TPFC_device_wiimote : public TPFC_device{
   private:
-    cwiid_wiimote_t *wiimote;	// wiimote handle 
+    // wiimote handle 
+    cwiid_wiimote_t *wiimote;
+    
     // estructura que contiene la información de los wiimotes
+    // para poder usar mas de 1 (el callback no sabe a que device esta
+    // asociado, asi que ha de recuperar un puntero al device a partir
+    // de la id del wiimote
     struct wiimoteinfo{
       int id;
       TPFC_device_wiimote* dev;
-      wiimoteinfo(){id=0; dev=NULL;};
+      // creadora
       wiimoteinfo(int i, TPFC_device_wiimote* d){id=i; dev=d;};
     };
     // vector que guarda la información de los wiimotes
     static vector<wiimoteinfo> wiimotes;
+
+    // Función que registra los wiimotes por su id, 
+    static void registerwiimote(cwiid_wiimote_t *, TPFC_device_wiimote* );
+    // Funcion que recupera un puntero al dev wiimote dada su id
+    static TPFC_device_wiimote* getwiimotedev(cwiid_wiimote_t *wiimote);
+
+
+    // funciones auxiliares de cwiid para configurar el report y suprimir errores
+    static void err(cwiid_wiimote_t *, const char *, va_list);
+    static void set_rpt_mode(cwiid_wiimote_t *, unsigned char);
 
 
   public:
@@ -28,13 +43,10 @@ class TPFC_device_wiimote : public TPFC_device{
     TPFC_device_wiimote(int ident);
     ~TPFC_device_wiimote();
 
+    // callback del wiimote
     static void callback(cwiid_wiimote_t *, int , union cwiid_mesg mesg[], struct timespec *);
     
-    static void err(cwiid_wiimote_t *, const char *, va_list);
-    static void set_rpt_mode(cwiid_wiimote_t *, unsigned char);
-
-    static void registerwiimote(cwiid_wiimote_t *, TPFC_device_wiimote* );
-    static TPFC_device_wiimote* getwiimotedev(cwiid_wiimote_t *wiimote);
+    
 };
 
 
