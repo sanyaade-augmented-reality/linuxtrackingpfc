05b68e7b5410b5368ae2215828e3035d141601d8
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-08 21:25:38 +0000

    3dfrom2d ahora funciona con angulos, solo esta implementada la profundidad fija

--- server/TPFC_device_3dfrom2d.C
+++ server/TPFC_device_3dfrom2d.C
@@ -6,6 +6,9 @@ TPFC_device_3dfrom2d::TPFC_device_3dfrom2d(int ident, TPFC_device* source):TPFC_
   data = new TrackingPFC_data(TPFCDATA3DORI);
   // La opción de merge esta activada por defecto
   merge = false;
+  // La opción por defecto es usar una profundidad fija
+  deep=FIJA;
+  dist = 1.0;
   // registramos este dispositivo en la lista de listeners del que vamos a usar como input
   source-> report_to(this);
 
@@ -43,24 +46,37 @@ void TPFC_device_3dfrom2d::report_from(TPFC_device* s){
       acumx=acumx/n;
       acumy=acumy/n;
       // guardamos los datos
-      data->setnewpos(acumx, acumy, 0.5);
+      setdata(acumx, acumy);
     }else{ // Los datos se envian como sensores separados
       // obtenemos el primer punto
       aux = sourcedata->getdata();
       // y lo guardamos en un nuevo report
-      data->setnewpos(aux[0], aux[1], 0.5);
-      float* aux2 = new float[3];
-      aux2[2]=0.5;
+      setdata(aux[0], aux[1]);
       // recorremos los siguientes puntos (si los hay)
       for (int i =1; i<n; i++){
 	// obtenemos y guardamos los datos de los puntos, pero añadiendolos al report existente
 	aux = sourcedata->getdata(i);
-	aux2[0]=aux[0];
-	aux2[1]=aux[1];
-	data->setmoredata(aux2);
+	setdata(aux[0], aux[1], false);
       }
     }
     // sea cual sea la opción, una vez escritos los datos, reportamos
     report();
   }
+}
+
+void TPFC_device_3dfrom2d::setdata(float x, float y, bool newrep){
+  float* aux= new float[3];
+
+  // Tipo de calculo de profundidad
+  if (deep==FIJA){
+    aux[0]=tan(x);
+    aux[1]=tan(y);
+    aux[2]=dist;
+  }
+  
+  // comprobamos si hay que empezar report nuevo o añadir al ya existente
+  if (newrep)
+    data->setnewdata(aux);
+  else
+    data->setmoredata(aux);
 }
