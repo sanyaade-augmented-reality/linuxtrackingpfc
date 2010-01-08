b46daa7af602057f2f5a755a140d7f2f453f3f68
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-08 10:42:06 +0000

    3d from 2d ya funciona con merge = false

--- server/TPFC_device_3dfrom2d.C
+++ server/TPFC_device_3dfrom2d.C
@@ -5,7 +5,7 @@ TPFC_device_3dfrom2d::TPFC_device_3dfrom2d(int ident, TPFC_device* source):TPFC_
   // creamos los datos
   data = new TrackingPFC_data(TPFCDATA3DORI);
   // La opción de merge esta activada por defecto
-  merge = true;
+  merge = false;
   // registramos este dispositivo en la lista de listeners del que vamos a usar como input
   source-> report_to(this);
 
@@ -25,21 +25,40 @@ void TPFC_device_3dfrom2d::report_from(TPFC_device* s){
     data->setnodata();
     nullreport();
   }else{// si son validos...
+    // obtenemos el numero de puntos del report
     int n = sourcedata->size();
+    const float* aux;
+
     if (merge){ // Hay que unificar todos los datos
+      // acumuladores
       float acumx=0;
       float acumy=0;
-      const float* aux;
+      // recorremos los puntos sumando los datos
       for (int i =0; i<n; i++){
 	aux = sourcedata->getdata(i);
 	acumx+=aux[0];
 	acumy+=aux[1];
       }
+      // obtenemos la media
       acumx=acumx/n;
       acumy=acumy/n;
+      // guardamos los datos
       data->setnewpos(acumx, acumy, 0.5);
     }else{ // Los datos se envian como sensores separados
-      
+      // obtenemos el primer punto
+      aux = sourcedata->getdata();
+      // y lo guardamos en un nuevo report
+      data->setnewpos(aux[0], aux[1], 0.5);
+      float* aux2 = new float[3];
+      aux2[2]=0.5;
+      // recorremos los siguientes puntos (si los hay)
+      for (int i =1; i<n; i++){
+	// obtenemos y guardamos los datos de los puntos, pero añadiendolos al report existente
+	aux = sourcedata->getdata(i);
+	aux2[0]=aux[0];
+	aux2[1]=aux[1];
+	data->setmoredata(aux2);
+      }
     }
     // sea cual sea la opción, una vez escritos los datos, reportamos
     report();
