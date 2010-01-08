42d157fa03fbdf7e5f10824fbc5c1541220e16f8
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-08 12:04:11 +0000

    el mainloop de vrpn del servidor ahora se realiza en un thread aparte

--- server/TPFC_server.C
+++ server/TPFC_server.C
@@ -9,6 +9,20 @@
 using namespace std;
 
 vrpn_Connection * connection;
+bool alive;
+pthread_t mainlooper;
+
+// thread que se encarga del mainloop del vrpn
+void* mainloop_thread(void* ){
+  while (alive){
+    if (connection!=NULL){
+      connection->mainloop();
+      vrpn_SleepMsecs(1); 
+    }else{
+      vrpn_SleepMsecs(1000); 
+    }
+  }
+}
 
 // funcion auxiliar para arrancar los Trackers
 void settracker(TPFC_device* dev, const char* name, int nsensors = 1){
@@ -18,27 +32,30 @@ void settracker(TPFC_device* dev, const char* name, int nsensors = 1){
     char  con_name[1024];
     sprintf(con_name, ":%d", port);
     connection = vrpn_create_server_connection(con_name, NULL, NULL);
+    // lanzamos el thread del mainloop
+    pthread_create( &mainlooper, NULL, mainloop_thread ,NULL);
   }
   // creamos el tracker en la conexion
   dev->settracker(connection, name, nsensors);
 }
 
 int main( int argc, char** argv ){
-    
-   connection=NULL;
-    
+    // incializaciones
+    connection=NULL;
+    alive = true;
+
     // Creamos los dispositivos del servidor
     vector<TPFC_device*> dev;
 
     // Facedetec
-    /*dev.push_back( new TPFC_device_opencv_face(0,0) );
-    dev.push_back( new TPFC_device_3dfrom2d(1,dev[0]) );
-    settracker(dev[1], "Tracker0");*/
-
-    // wiimote
-    dev.push_back( new TPFC_device_wiimote(0) );
+    dev.push_back( new TPFC_device_opencv_face(0,0) );
     dev.push_back( new TPFC_device_3dfrom2d(1,dev[0]) );
     settracker(dev[1], "Tracker0");
+
+    // wiimote
+    /*dev.push_back( new TPFC_device_wiimote(0) );
+    dev.push_back( new TPFC_device_3dfrom2d(1,dev[0]) );
+    settracker(dev[1], "Tracker0",4);*/
 
     /*dev.push_back( new TPFC_device_wiimote(0) );
     dev.push_back( new TPFC_device_wiimote(1) );
@@ -47,26 +64,19 @@ int main( int argc, char** argv ){
     dev.push_back( new TPFC_device_3dfrom2d(3,dev[1]) );
     settracker(dev[3], "Tracker1");*/
 
-    
-    
-    
-
-    while (1){
-      if (connection!=NULL)
-	connection->mainloop();
-      vrpn_SleepMsecs(1);
-      
-    }
-    //char x = getchar();
+    // placeholder para el bucle principal
+    char x = getchar();
 
     // paramos los devices
     for (int i =0; i<dev.size();i++){
        dev[i]->stop();
     }
-
-
-    
-    
+    // desactivamos el flag para que se detenga el thread del mainloop
+    alive=false;
+    // esperamos a que pare el mainloop (si hay servidor)
+    if (connection!=NULL)
+      pthread_join( mainlooper, NULL);
+    // finalizamos
     return 0;
 }
 
