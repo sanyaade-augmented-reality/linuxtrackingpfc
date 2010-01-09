7decc0ead2e7b4abc29128f2f529da677a31af52
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-09 21:58:43 +0000

    ahora se destruyen las copias de los datachunks que se usan

--- server/TPFC_device_3dstereo.h
+++ server/TPFC_device_3dstereo.h
@@ -6,13 +6,23 @@
 class TPFC_device_3dstereo : public TPFC_device{
    private:
     TPFC_device** sources;
-    
+
+    bool calibrated; // flag de calibrado
+    void calibrate(); // funcion para calibrar
+    float* calib_data; // buffer que se usa durante el calibrado
+    int calib_samples; // numero de muestras en cada punto para calibrar
+    int calib_count; // total de puntos almacenados en calib_data (indice)
+    int calib_dots; // numero de puntos simultaneos durante el calibrado
+    pthread_mutex_t* calib_lock;
+    void adddot(float,float,float,float,int);
+
     public:
     // consctructora y creadora
     TPFC_device_3dstereo(int ident, TPFC_device*,TPFC_device*);
     ~TPFC_device_3dstereo();
     // sobrecarga de report from, que en este caso es la que realizará los calculos del device
     void report_from(TPFC_device*);
+    void nullreport_from(TPFC_device*);
     
     // funcion que devuelve en un string la información relativa al dispositivo
     string info();
