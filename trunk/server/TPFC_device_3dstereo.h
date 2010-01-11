6da9886d6ec6befaef7050b5c604541fa2bde11b
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-11 17:19:53 +0000

    Eliminado toda variable y funcion referente al calibrado el device en si ya funciona, solo falta guardar los puntos bien si hay mas de uno en el report

--- trunk/server/TPFC_device_3dstereo.h
+++ trunk/server/TPFC_device_3dstereo.h
@@ -13,6 +13,7 @@ class TPFC_device_3dstereo : public TPFC_device{
     // semaforo para exclusion mutua en el acceso a lastdata
     pthread_mutex_t* lock;
 
+    double camdist; // distancia entre ambos sensores
     int left; // indice del sensor izquierdo
 
     // funcion que encuentra el indice interno de la fuente (si esta en sources[0] o sources[1]
@@ -21,28 +22,10 @@ class TPFC_device_3dstereo : public TPFC_device{
     // a un angulo usable para coordenar (el angulo interior del triangulo usado)
     double angleconversion(double, int);
     
-
-    // variables usadas en la calibracion
-    bool calibrated; // flag de calibrado
-    double* calib_data; // buffer que se usa durante el calibrado. los datos estan almacenados
-		       // en tramos de calib_sample longitud, en este orden:
-		       // x_wm1_dot1, y_wm1_dot1, x_wm2_dot1, y_wm2_dot1,
-		       // x_wm1_dot2, y_wm1_dot2, x_wm2_dot2, y_wm2_dot2... 
-    int calib_count; // total de puntos almacenados en calib_data (indice)
-    pthread_mutex_t* calib_lock;
-    void addsample(double*); // funcion para añadir los datos de una muestra al bufer
-    double* getsamples(int, int, int); // funcion para recuperar todos los samples del buffer
-
     public:
     // consctructora y creadora
     TPFC_device_3dstereo(int ident, TPFC_device*,TPFC_device*);
     ~TPFC_device_3dstereo();
-
-    // configuracion de la calibracion
-    int calib_samples; // numero de muestras en cada punto para calibrar
-    int calib_dots; // numero de puntos simultaneos durante el calibrado
-    //int calib_discard; // % de las muestras a descartar al procesar los datos
-    void calibrate(); // funcion para calibrar
 
     // sobrecarga de report from, que en este caso es la que realizará los calculos del device
     void report_from(TPFC_device*);
