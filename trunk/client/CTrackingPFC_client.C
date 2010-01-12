b310fa6029d1ae4246ceaca6fd034966bf1a2401
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-12 19:15:35 +0000

    añadido CTrackingPFC_client para usarse de puente entre C++ y C

---
+++ trunk/client/CTrackingPFC_client.C
@@ -0,0 +1,44 @@
+#include "CTrackingPFC_client.h"
+#include "TrackingPFC_client.h"
+
+extern "C" {
+
+
+/*class TrackingPFC_client{
+  public:
+    // Creadoras y destructora
+    //TrackingPFC_client(); // No hace falta, la de abajo la suple
+    TrackingPFC_client(const char* tname="Tracker0@localhost", void(TrackingPFC_client*)=NULL);
+    ~TrackingPFC_client();
+    
+    // Devuelve el tamaño de la pantalla (horizontal)
+    float getDisplaySizex();
+    //float getDisplaySizey();
+    //float getDisplayRatio();
+
+    // consultoras y escritoras de los datos
+    float* getlastpos();
+    
+};*/
+
+// Implementacion de las funciones basicas en C
+
+CTrackingPFC_client* tpfcclient(const char* tname){
+  return (CTrackingPFC_client*) (new TrackingPFC_client(tname));
+}
+
+
+void tpfccdelete(CTrackingPFC_client* c){
+  TrackingPFC_client* client = (TrackingPFC_client*)c;
+  free(c);
+}
+
+float tpfccgetDisplaySizex(CTrackingPFC_client* c){
+  return ((TrackingPFC_client*)c)->getDisplaySizex();
+}
+float* tpfccgetlastpos(CTrackingPFC_client* c){
+  return ((TrackingPFC_client*)c)->getlastpos();
+}
+
+
+}
