5458b3f32723247bfaf2721a06913a88d92991d2
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-13 19:19:00 +0000

    Añadida la demo de blender Se ha integrado un TPFC_client en G, se inicializa y se destruye correctamente Se ha modificado la parte encargada de configurar el frustum, falta: conseguir refresco de pantalla, pasar el originalz hasta la funcion que ha de mover la camara( y modificar la camara), conseguir la escala

---
+++ demos/blender/get.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# para incluir el tracker en la declaracion de G
+cp sources/blenkernel/BKE_global.h ./
+# para inicializar el tracker a la vez que G
+cp sources/blenkernel/intern/blender.c ./
+# para destruir el tracker antes de salir, sin provocar un segfault
+cp sources/src/usiblender.c ./
+
+cp sources/include/BIF_mywindow.h ./
+cp sources/src/mywindow.c ./
+cp sources/src/view.c ./
