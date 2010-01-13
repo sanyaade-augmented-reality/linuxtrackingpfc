e258acda9dc5b8d7b4ad4771a39f64240628a6de
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-13 16:02:28 +0000

    Ahora el cliente puede leer la resolucion y el tamaño del display directamente de las x añadidas funciones para obtener esos 4 valores las funciones viejas para obtener el tamaño siguen vigentes, pero si no encuentran el archivo de configuracion, usaran los valores obtenidos de las X

--- client/CTrackingPFC_client.h
+++ client/CTrackingPFC_client.h
@@ -3,29 +3,14 @@ typedef void CTrackingPFC_client;
 extern "C" {
 #endif
 
-/*class TrackingPFC_client{
-  public:
-    // Creadoras y destructora
-    //TrackingPFC_client(); // No hace falta, la de abajo la suple
-    TrackingPFC_client(const char* tname="Tracker0@localhost", void(TrackingPFC_client*)=NULL);
-    ~TrackingPFC_client();
-    
-    // Devuelve el tamaño de la pantalla (horizontal)
-    float getDisplaySizex();
-    //float getDisplaySizey();
-    //float getDisplayRatio();
-
-    // consultoras y escritoras de los datos
-    float* getlastpos();
-    
-};*/
-
 // Implementacion de las funciones basicas en C
 
 CTrackingPFC_client* tpfcclient(const char* tname);
 void tpfccdelete(CTrackingPFC_client* c);
 float tpfccgetDisplaySizex(CTrackingPFC_client* c);
 float tpfccgetDisplaySizey(CTrackingPFC_client* c);
+int tpfccgetDisplayWidth(CTrackingPFC_client* c);
+int tpfccgetDisplayHeight(CTrackingPFC_client* c);
 float* tpfccgetlastpos(CTrackingPFC_client* c);
 
 #ifdef __cplusplus
