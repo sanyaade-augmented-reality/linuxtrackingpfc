ae8b71cc635b99da8cda26378b49e57b225e52bd
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-15 17:55:27 +0000

    añadida estructura basica de 3dpattern

---
+++ trunk/server/TPFC_device_3dpattern.C
@@ -0,0 +1,37 @@
+#include "TPFC_device_3dpattern.h" 
+
+
+TPFC_device_3dpattern::TPFC_device_3dpattern(int ident, TPFC_device* s):TPFC_device(ident){
+  // creamos los datos
+  data = new TrackingPFC_data(TrackingPFC_data::TPFCDATA3DORI,100);
+  // guardamos un puntero a la fuente
+  source=s;
+  // registramos este dispositivo en la lista de listeners del que vamos a usar como input
+  s-> report_to(this);
+  
+}
+
+TPFC_device_3dpattern::~TPFC_device_3dpattern(){
+  free(data);
+}
+
+// sobrecarga del handler de los reports
+void TPFC_device_3dpattern::report_from(TPFC_device* s){
+}
+
+
+// Informacion sobre el dispositivo
+string TPFC_device_3dpattern::info(){
+  char aux[64]; // si se crean MUCHOS dispositivos nos saldremos del buffer... dudo que sea un problema
+  sprintf(aux, "3dpattern (usando de fuente el dispositiv %i)", source->idnum());
+  return aux;
+}
+
+// funcion que comprueba si el dispositivo s es una fuente valida para este dispositivo
+// devuelve "ok" si es correcta, o una string con la informacion relevante si no lo es
+string TPFC_device_3dpattern::checksource(TPFC_device* s){
+  string ret = "ok";
+  if (s->getdata()->datatype()!=TrackingPFC_data::TPFCDATA3D && s->getdata()->datatype()!=TrackingPFC_data::TPFCDATA3DORI)
+    ret ="El tipo de datos de la fuente no es el adecuado: debe ser un dispositivo 3d (tenga o no orientacion).\n";
+  return ret;
+}
---
+++ trunk/server/TPFC_device_3dpattern.h
@@ -0,0 +1,29 @@
+#ifndef TPFC_DEVICE_3DPATTERN_
+#define TPFC_DEVICE_3DPATTERN_
+
+#include "TPFC_device.h"
+
+
+class TPFC_device_3dpattern : public TPFC_device{
+  private:
+    // Puntero a los 2 device fuente
+    TPFC_device* source;
+  public:
+    // consctructora y creadora
+    TPFC_device_3dpattern(int ident, TPFC_device*);
+    ~TPFC_device_3dpattern();
+
+    // sobrecarga de report from, que en este caso es la que realizará los calculos del device
+    void report_from(TPFC_device*);
+
+    // funcion que devuelve en un string la información relativa al dispositivo
+    string info();
+
+    // funcion que comprueba si el dispositivo s es una fuente valida para este dispositivo
+    // devuelve "ok" si es correcta, o una string con la informacion relevante si no lo es
+    // debe ser definida por todas las clases que hereden de device
+    // (aunque no se puede hacer virtual ya que es estatica)
+    static string checksource(TPFC_device*);
+};
+
+#endif /*TPFC_DEVICE_3DPATTERN_*/
