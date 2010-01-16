9faae7b2d7e77f478ce42d8a3f9f6a0340977f13
plaganocturna <plaganocturna@e4469068-ad03-11de-9815-ff96df1888ed>
2010-01-16 11:33:05 +0000

    version "bastante" funcional con all=false

--- trunk/server/TPFC_device_3dpattern.C
+++ trunk/server/TPFC_device_3dpattern.C
@@ -16,8 +16,8 @@ TPFC_device_3dpattern::TPFC_device_3dpattern(int ident, TPFC_device* s, int dot,
 
   tolerance=0.2;// 20%
   // posiciones del ultimo patron encontrado
-  // { 0{xyz} 1{xyz} [2{xyz}] }
-  lastpattern= new double[3*dots];
+  // { pos{xyz} ori{xyzw} dot0{xyz} dot1{xyz} [dot2{xyz}]}
+  lastpattern= new double[7+3*dots];
   anypattern=false; // aun no hemos encontrado patrones
   
   // guardamos un puntero a la fuente
@@ -48,11 +48,11 @@ void TPFC_device_3dpattern::report_from(TPFC_device* s){
       // creamos un vector de bools[n] para saber si los dots forman parte o no del patron
       bool included[n];
       for (int dn =0; dn<n; dn++)
-	included[n]=false;
+	included[dn]=false;
       
       bool foundpattern=false; // flag que dice si el report tiene datos validos
       if (n>=dots || all==false){// si requerimos todos los puntos y no hay suficientes, no entramos
-	// y un contador de pares de puntos que estan a la distancia correcta
+	//contador de pares de puntos que estan a la distancia correcta
 	int distok=0;
 	// vamos punto por punto comprobando si entran dentro de la distancia y la tolerancia
 	const double* dot1;
@@ -94,9 +94,9 @@ void TPFC_device_3dpattern::report_from(TPFC_device* s){
 	      newdot[1]+=aux[1];
 	      newdot[2]+=aux[2];
 	      // actualizamos lastpattern
-	      lastpattern[pdn*3]=aux[0];
-	      lastpattern[pdn*3+1]=aux[1];
-	      lastpattern[pdn*3+2]=aux[2];
+	      lastpattern[7+pdn*3]=aux[0];
+	      lastpattern[7+pdn*3+1]=aux[1];
+	      lastpattern[7+pdn*3+2]=aux[2];
 	      pdn++;
 	    }
 	  }
@@ -109,12 +109,15 @@ void TPFC_device_3dpattern::report_from(TPFC_device* s){
 	  newdot[4]=0;
 	  newdot[5]=0;
 	  newdot[6]=0;
+	  // guardamos la informacion en lastpattern
+	  for (int i =0; i<7;i++)
+	    lastpattern[i]=newdot[i];
 	  // guardamos el punto
 	  data->setnewdata(newdot);
 	  // y lo marcamos
 	  data->settag(tag);
 	  //liberamos el vector auxiliar
-	  free (newdot);
+	  //free (newdot);
 	  // marcamos el flag a cierto ya que hemos incluido datos
 	  foundpattern = true;
 	} // hay suficientes numeros para el patron
@@ -122,13 +125,101 @@ void TPFC_device_3dpattern::report_from(TPFC_device* s){
 	// buscamos coincidencias parciales
 	// solo entramos aqui si el problema no es que hemos encontrado puntos DE MAS
 	// no entramos si aun no hemos encontrado un pattern en ningun report anterior
-	if ( !foundpattern && !all &&  ( (dots==2 && distok==0) || (dots==3 && distok<3) && anypattern)){
+	if ( !foundpattern && !all && anypattern && ( (dots==2 && distok==0) || (dots==3 && distok<3))){
 	  // recorremos los puntos actuales buscando cuales estan mas cerca de los del ultimo pattern
-	  // si tenemos un par nos quedamos con ellos como referencia, 
-	  // si no, nos quedamos al/los que tenga menos distancia (hay que definir un umbral)
-	  // calculamos el vectores desde lastpattern
-	  // aplicamos esos vectores al punto actual
-	  // ya tenemos el punto
+	  double distances[dots];
+	  int nearest[dots];
+	  float dotdis;// auxiliar para la distancia entre puntos
+	  int p1=-1;
+	  int p2=-1;
+	  for (int dn=0; dn<n;dn++){ // dot number
+	    const double* aux = sourcedata->getdata(dn);
+	    for (int li=0; li<dots;li++){ // last index
+	      dotdis=dotdist(aux,&lastpattern[7+3*li]);
+	      if (dn==0 || dotdis<distances[li]){
+		distances[li]=dotdis;
+		nearest[li]=dn;
+	      }
+	    }
+	    if (included[dn]){
+	      // si este punto antes estaba marcado como parte del patron, lo guardamos
+	      if (p1!=-1) p2=dn;
+	      else p1=dn;
+	    }
+	  }
+
+	  // si no habia ningun par usable, nos quedamos el que tenga menos distancia,
+	  // siempre que esté dentro del umbral
+	  int last =0; //indice en lastpattern donde está el punto relacionado
+	  if (p1==-1){
+	    // encontramos que distancia es la menor
+	    if (distances[1]<distances[last])
+	      last=1;
+	    if (dots==3 && distances[2]<distances[last])
+	      last=2;
+	    // distances[last] es la menor de las 3
+	    // como umbral definimos el tamaño del pattern
+	    if (distances[last]<dist)
+	      p1=nearest[last]; // guardamos el numero del punto mas cercano al anterior
+	  }else{ // p1 y p2 son pares usables, comprobamos si tienen correspondencia con el ultimo pattern
+	    if ( !(p1==nearest[0] || p1==nearest[1] || p1==nearest[2]) )
+	      p1=-1;
+	    if ( !(p2==nearest[0] || p2==nearest[1] || p2==nearest[2]) )
+	      p2=-1;
+	    // si p2 es valido y p1 no, guardamos p2
+	    if (p1==-1 && p2!=-1)
+	      p1=p2;
+	    // actualizamos en last cual es el punto relacionado
+	    if (p1==nearest[1])
+	      last=1;
+	    else if (p1==nearest[2])
+	      last=2;
+	  }
+
+	  // llegados aqui, si p1=-1 es que no habia puntos utiles
+	  if (p1!=-1){
+	    // en last tenemos el indice del punto relacionado de lastpattern
+	    // calculamos el vector desde ese punto al centro de su patron
+	    double* newdot= new double[7];
+	    /*newdot[0]=lastpattern[0]-lastpattern[7+last*3];
+	    newdot[1]=lastpattern[1]-lastpattern[8+last*3];
+	    newdot[2]=lastpattern[2]-lastpattern[9+last*3];
+	    // sumamos el punto actual a ese vector
+	    const double* aux = sourcedata->getdata(nearest[last]);
+	    newdot[0]+=aux[0];
+	    newdot[1]+=aux[1];
+	    newdot[2]+=aux[2];*/
+	    const double* aux = sourcedata->getdata(nearest[last]);
+	    newdot[0]=aux[0]-lastpattern[7+last*3];
+	    newdot[1]=aux[1]-lastpattern[8+last*3];
+	    newdot[2]=aux[2]-lastpattern[9+last*3];
+	    // sumamos el vector actual al centro del ultimo pattern
+	    newdot[0]+=lastpattern[0];
+	    newdot[1]+=lastpattern[1];
+	    newdot[2]+=lastpattern[2];
+	    // copiamos la orientacion del vector pasado
+	    newdot[3]=lastpattern[3];
+	    newdot[4]=lastpattern[4];
+	    newdot[5]=lastpattern[5];
+	    newdot[6]=lastpattern[6];
+
+	    /*// actualizamos lastpattern
+	    for (int i =0; i<7;i++)
+	      lastpattern[i]=newdot[i];
+	    for (int li=0;li<dots;li++){
+	      
+	    }*/
+
+	    // guardamos el punto, pero lo marcamos como artificial
+	    data->setnewdata(newdot, false);
+	    // y lo marcamos
+	    data->settag(tag);
+	    //liberamos el vector auxiliar
+	    //free (newdot);
+	    // marcamos el flag a cierto ya que hemos incluido datos
+	    foundpattern = true;
+	    
+	  }
 	}
       } //if (n>=dots || all=false
       // si hay que incluir los puntos que no pertenezcan al patron, los guardamos en data
